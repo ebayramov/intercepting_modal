0d2c83a5bf5363a957447ca05cc6571dfad72e0b
ebayramov <ebayramov06@gmail.com>
2024-03-10 17:14:50 +0400

    Second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    basePath: '/intercepting_modal',
+    assetPrefix: '/intercepting_modal/'
+  };
+
 export default nextConfig;
+
+
